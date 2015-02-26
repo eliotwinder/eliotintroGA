f765f4d83d25086a9328e9700eb60ed27ce5af9b
eliotwinder <musicismymistress@gmail.com>
2015-02-26 14:47:59 -0800

    first upload

---
+++ eliotintro.js
@@ -0,0 +1,23 @@
+$(function() {
+    $('#work,#education,#links').hide();
+
+    $('#sum').click(function(){
+    	$('#work,#education,#links').hide()
+    	$('#summary').show();
+    });
+
+    $('#wor').click(function(){
+    	$('#summary,#education,#links').hide()
+    	$('#work').show();
+    });
+
+    $('#edu').click(function(){
+    	$('#summary,#work,#links').hide()
+    	$('#education').show();
+    });
+
+    $('#lin').click(function(){
+    	$('#summary,#work,#education').hide()
+    	$('#links').show();
+    });
+});
